c0fc80772b62d88811202600cebde5e71ca3216b
Jared Gutkin <jgutkinsam@gmail.com>
2022-07-06 20:48:21 -0500

    updated react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import { useEffect } from 'react';
+
+//f36bada8
+
+const API_URL = 'http://omdbapi.com?apikey=f36bada8'
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        console.log(data.Search)
+    }
+
+    useEffect(()=>{
+        searchMovies('Evil Dead');
+    }, [])
+
     return (
         <h1>App</h1>
     );
